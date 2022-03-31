558da498df7409de8472b324ea2ddf4882a85348
Brian Fitzgerald <brian.fitzgerald@equipmentshare.com>
2022-03-30 21:54:55 -0500

    init

---
+++ main.py
@@ -0,0 +1,44 @@
+from email.policy import default
+from os import walk
+import os
+from tree_sitter import Language, Parser
+from pydriller import Repository
+
+Language.build_library(
+  # Store the library in the `build` directory
+  'build/my-languages.so',
+
+  # Include one or more languages
+  [
+    'tree-sitter-python'
+  ]
+)
+
+PY_LANGUAGE = Language('build/my-languages.so', 'python')
+
+parser = Parser()
+parser.set_language(PY_LANGUAGE)
+
+# For each file, parse the AST for methods and classes.
+# Get a delta of methods and classes for each commit.
+# For each AST entry generate a history.
+
+classes = {}
+
+cwd = os.getcwd()
+
+for commit in Repository(cwd).traverse_commits():
+    for m in commit.modified_files:
+        if m.new_path.endswith('.py'):
+            source = m.source_code
+            try:
+                tree = parser.parse(bytes(source, 'utf-8'))
+                print(tree.sexp())
+            except Exception as e:
+                print(e)
+                continue
+
+
+
+
+# CLI for querying a specific class and method - find all uses and when each use was added.
---
+++ test.py
@@ -0,0 +1,7 @@
+class Bungus(object):
+    
+    def __init__(self, name):
+        self.name = name
+
+    def say_name(self):
+        print("my name is {}".format(self.name))
